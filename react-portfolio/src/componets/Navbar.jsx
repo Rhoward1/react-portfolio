a89cfa9332d2571ab6a5ba84c03686ca6470f516
Rhoward1 <ryanmhoward@gmail.com>
2022-06-13 11:15:38 -0500

    updating

--- react-portfolio/src/componets/Navbar.jsx
+++ react-portfolio/src/componets/Navbar.jsx
@@ -1,13 +1,14 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/logo.png'
 import { FaBars, FaHamburger, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa'
-import {HiOutlineMail} from 'react-icons/hi'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { Link } from 'react-scroll'
 
 
 const Navbar = () => {
-    const [nav, setNav]= useState(false)
-    const handleClick =() => setNav(!nav)
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
 
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
@@ -17,51 +18,92 @@ const Navbar = () => {
             {/* menu */}
             <div className='hidden md:flex' >
                 <ul className='hidden md:flex'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Skills</li>
-                    <li>My Work</li>
-                    <li>Contact</li>
+                    <li>
+                        <Link to="home" smooth={true} duration={500}>
+                            Home
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to="about" smooth={true} duration={500}>
+                            About Me
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="Skills" smooth={true} duration={500}>
+                            My Skills
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="work" smooth={true} duration={500}>
+                            My Work
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="contact" smooth={true} duration={500}>
+                            Contact
+                        </Link>
+                    </li>
                 </ul>
             </div>
             {/* Hamburger */}
 
             <div onClick={handleClick} className='md:hidden z-10'>
-               {!nav ? <FaBars /> : <FaTimes />}
+                {!nav ? <FaBars /> : <FaTimes />}
             </div>
             {/* Mobile Menu */}
             <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-                <li className='py-6 text-4xl'>Home</li>
-                <li className='py-6 text-4xl'>About</li>
-                <li className='py-6 text-4xl'>Skills</li>
-                <li className='py-6 text-4xl'>My Work</li>
-                <li className='py-6 text-4xl'>Contact</li>
+                <li  className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                        About Me
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="Skills" smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+                        My Work
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
             {/* Social icons */}
             <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
-                        href='/'>
-                            Linkedin <FaLinkedin size={30}/>
+                        <a className='flex justify-between items-center w-full text-gray-300' target="_blank" rel="noopener noreferrer"
+                            href='https://www.linkedin.com/in/ryan-m-howard/'>
+                            Linkedin <FaLinkedin size={30} />
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
-                        href='/'>
-                            GitHub <FaGithub size={30}/>
+                        <a className='flex justify-between items-center w-full text-gray-300' target="_blank" rel="noopener noreferrer"
+                            href='https://github.com/Rhoward1'>
+                            GitHub <FaGithub size={30} />
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0f974a]'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
-                        href='/'>
-                            Email Me <HiOutlineMail size={30}/>
+                        <a className='flex justify-between items-center w-full text-gray-300' target="_blank" rel="noopener noreferrer"
+                            href='mailto:ryanmhoward.dev@gmail.com'>
+                            Email Me <HiOutlineMail size={30} />
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#435161]'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
-                        href='/'>
-                            My Resume <BsFillPersonLinesFill size={30}/>
+                        <a className='flex justify-between items-center w-full text-gray-300' target="_blank" rel="noopener noreferrer"
+                            href='https://docs.google.com/document/d/10spHm2V6kEE9i_4XNpgDTNrN-G4auOVhIscZTxYD-iE/edit?usp=sharing'>
+                            My Resume <BsFillPersonLinesFill size={30} />
                         </a>
                     </li>
                 </ul>
